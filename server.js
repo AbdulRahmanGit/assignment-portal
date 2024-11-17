a7854b395b8a950090556e96dae963c849ff003e
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2024-11-17 22:36:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -96,16 +96,16 @@ const getWelcomePage = () => `
 
         <h3>User Operations:</h3>
         <ul>
-            <li><span class="method post">POST</span> /api/users/upload - Upload an assignment</li>
+            <li><span class="method post">POST</span> /api/users/upload - Upload an assignment</li><em>Protected</em>
             <li><span class="method get">GET</span> /api/users/admins - Fetch all admins</li>
-            <li><span class="method get">GET</span> /api/users/assignments - Fetch assignments completed</li>
+            <li><span class="method get">GET</span> /api/users/assignments - Fetch assignments completed</li><em>Protected</em>
         </ul>
 
         <h3>Admin Operations:</h3>
         <ul>
-            <li><span class="method get">GET</span> /api/admin/assignments - View assignments</li>
-            <li><span class="method post">POST</span> /api/admin/assignments/:id/accept - Accept assignment</li>
-            <li><span class="method post">POST</span> /api/admin/assignments/:id/reject - Reject assignment</li>
+            <li><span class="method get">GET</span> /api/admin/assignments - View assignments</li><em>Protected</em>
+            <li><span class="method post">POST</span> /api/admin/assignments/:id/accept - Accept assignment</li><em>Protected</em>
+            <li><span class="method post">POST</span> /api/admin/assignments/:id/reject - Reject assignment</li><em>Protected</em>
         </ul>
         
         <p style="margin-top: 20px; color: #666;">
