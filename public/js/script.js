3c0277174030eaf3347f5f2a651be89c28fe408c
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2024-11-17 18:38:51 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('/api/auth/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
