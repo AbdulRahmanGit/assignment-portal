1e96606178e4cd814253b95b8815de3c2d827cdd
Abdul Rahman <102407007+AbdulRahmanGit@users.noreply.github.com>
2024-12-03 19:11:26 +0530

    Update googleCloud.js

--- config/googleCloud.js
+++ config/googleCloud.js
@@ -1,8 +1,9 @@
 const { Storage } = require('@google-cloud/storage');
-
+const googleCloudKey = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 const storage = new Storage({
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  credentials: googleCloudKey
 });
 
 const bucket = storage.bucket(process.env.GOOGLE_CLOUD_BUCKET_NAME);
